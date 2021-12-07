8faed26df770de656c48aa2a539a2fce7db2d1f3
Kenneth Shaw <kenshaw@gmail.com>
2021-12-07 07:08:19 +0700

    Fleshing out the API

--- ccookies.go
+++ ccookies.go
@@ -43,9 +43,9 @@ CREATE TABLE cookies(
 )
 */
 
-// Read reads the cookies from the provided sqlite3 file on disk.
-func Read(file, host string) ([]*http.Cookie, error) {
-	// check sqlite driver available
+// ReadContext reads the cookies from the provided sqlite3 file on disk.
+func ReadContext(ctx context.Context, file, host string) ([]*http.Cookie, error) {
+	// check sqlite driver
 	driver := driverName()
 	if driver == "" {
 		return nil, errors.New("code using ccookies must import a sqlite driver!")
@@ -56,31 +56,26 @@ func Read(file, host string) ([]*http.Cookie, error) {
 		return nil, err
 	}
 	defer db.Close()
-	// determine retrieval
+	// query func and params
 	f := models.Cookies
 	if host != "" {
 		f, host = models.CookiesLikeHost, "%"+strings.TrimPrefix(host, "%")
 	}
-	// read and convert
-	res, err := f(context.Background(), db, host)
+	// exec and convert
+	res, err := f(ctx, db, host)
 	if err != nil {
 		return nil, err
 	}
 	return models.Convert(res), nil
 }
 
-// ReadCookieJar reads the cookies from the provided sqlite3 file for the
-// provided url, returning a cookie jar usable with http.Client.
-func ReadCookieJar(file, urlstr string) (http.CookieJar, error) {
-	// read cookies for url host
-	u, err := url.Parse(urlstr)
-	if err != nil {
-		return nil, err
-	}
-	cookies, err := Read(file, u.Host)
-	if err != nil {
-		return nil, err
-	}
+// Read reads the cookies from the provided sqlite3 file on disk.
+func Read(file, host string) ([]*http.Cookie, error) {
+	return ReadContext(context.Background(), file, host)
+}
+
+// Jar builds a cookie jar for the url from provided cookies.
+func Jar(u *url.URL, cookies ...*http.Cookie) (http.CookieJar, error) {
 	// build jar
 	jar, err := cookiejar.New(&cookiejar.Options{
 		PublicSuffixList: publicsuffix.List,
@@ -90,6 +85,44 @@ func ReadCookieJar(file, urlstr string) (http.CookieJar, error) {
 	}
 	jar.SetCookies(u, cookies)
 	return jar, nil
+}
+
+// ReadJar reads the cookies from the provided sqlite3 file for the provided
+// url into a cookie jar usable with http.Client.
+func ReadJar(file, urlstr string) (http.CookieJar, error) {
+	// read cookies
+	u, err := url.Parse(urlstr)
+	if err != nil {
+		return nil, err
+	}
+	cookies, err := Read(file, u.Host)
+	if err != nil {
+		return nil, err
+	}
+	return Jar(u, cookies...)
+}
+
+// ReadJarFiltered reads the cookies from the provided sqlite3 file for the
+// provided url into a cookie jar (usable with http.Client) consisting of of
+// cookies passed through filter func f.
+func ReadJarFiltered(file, urlstr string, f func(*http.Cookie) bool) (http.CookieJar, error) {
+	// read cookies
+	u, err := url.Parse(urlstr)
+	if err != nil {
+		return nil, err
+	}
+	cookies, err := Read(file, u.Host)
+	if err != nil {
+		return nil, err
+	}
+	// filter
+	var c []*http.Cookie
+	for _, cookie := range cookies {
+		if f(cookie) {
+			c = append(c, cookie)
+		}
+	}
+	return Jar(u, c...)
 }
 
 // driverName returns the first sqlite3 driver name it encounters.
